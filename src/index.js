024141dc38060c5b5b676f4b8eba3f5eb46987c2
Kirill Mokevnin <mokevnin@gmail.com>
2017-05-25 08:30:24 +0300

    update docs

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import dispatch from './dispatcher';
 
 /**
  * Perform get request
+ *
+ * @example
+ * get(host).then(response => console.log(response.body))
+ *
  */
 export const get = (url: string, config: Object = {}) =>
   dispatch({ ...config, url, method: 'GET' });
 
 /**
  * Perform post request
+ *
+ * @example
+ * post(host, { key: 'value' }).then(response => console.log(response.status))
  */
 export const post = (url: string, data: Object, config: Object = {}) =>
   dispatch({ ...config, url, data, method: 'POST' });
